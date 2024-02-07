2b3b4f43a750411d96d01ab475124775912e532a
NeerajAuchar <neerajauchar@gmail.com>
2024-02-07 20:13:15 +0530

    que3 done

---
+++ arrays_and_strings/src/que3.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class que3 {
+    public static void main(String[] args) {
+        String s =" Neeraj Auchar ";
+        replaceSpaces(s, s.length());
+
+    }
+
+    static void q3(String s){
+       String news = s.replace(" ", "%20");
+        System.out.println(news);
+    }
+    static void replaceSpaces(String str , int trueLength){
+        char[] arr = str.toCharArray();
+        int spaceCount =0 , index ;
+        for (int i = 0 ; i <trueLength ;i++ ){
+          if (arr[i] == ' '){
+              spaceCount++;
+          }
+        }
+        index = trueLength +spaceCount *2;
+        if (trueLength < arr.length) arr[trueLength] ='\0';
+        for ( int i = trueLength-1 ; i <= 0 ; i-- ){
+            if (arr[i] == ' '){
+                arr[index -1] = '0';
+                arr[index -2] = '2';
+                arr[index -3] = '%';
+                index = index -3;
+            }else {
+                arr[index -1] = arr[i];
+                index--;
+            }
+        }
+        String newarr = arr.toString();
+        System.out.println(newarr);
+    }
+
+}
